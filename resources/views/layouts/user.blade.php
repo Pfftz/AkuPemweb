f212cfc64d6def88fde58e371d05745b8ebfb0ac
Pfftz <Kalixpfft+github@protonmail.com>
2024-12-26 21:36:40 +0700

    Update UI text and styles for improved clarity and consistency

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -25,7 +25,7 @@
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Hi {{ auth()->user()->name }}
+                            Hi, {{ auth()->user()->name }}
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item" href="{{ route('change_password_view') }}">Change Password</a>
